d802cac4f442534bc68ba448f13b317592c47711
vishal <vishalambekar459@gmail.com>
2022-09-01 01:17:00 +0530

    method references code added

--- java8AndAbove/src/Tutorial2/methodReferences/MethodReferencesDemo.java
+++ java8AndAbove/src/Tutorial2/methodReferences/MethodReferencesDemo.java
@@ -1,5 +1,10 @@
 package Tutorial2.methodReferences;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -27,9 +32,11 @@ public class MethodReferencesDemo {
 		//using method reference
 		Function<Integer, Double> function2  = Math::sqrt;
 		System.out.println(function2.apply(16));
+		System.out.println();System.out.println();
 		
 		
-		//1) BIFUNCTION functional interface/////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		//1) BIFUNCTION functional interface//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		
 		BiFunction<Integer, Integer, Integer> biFunctionLambda = (a,b) -> MethodReferencesDemo.addition(a, b);
 		System.out.println("bifunctionLambda sum :: "+biFunctionLambda.apply(3, 4));
@@ -37,9 +44,13 @@ public class MethodReferencesDemo {
 		//se method reference
 		BiFunction<Integer, Integer, Integer> biFunctionLambda2 = MethodReferencesDemo::addition;
 		System.out.println("bifunctionLambda sum :: "+biFunctionLambda2.apply(5, 4));
+		System.out.println();System.out.println();
 		
 		
-		// 2) Method reference to an instance method of an object////////////////////////////////////////////////////////////////////////////////////
+		
+		
+		
+		// 2) Method reference to an instance method of an object////////////////////////////////////////////////////////////////////////////////////////////////
 		MethodReferencesDemo methodReferencesDemo = new MethodReferencesDemo();
 		
 		//normal lambda
@@ -49,10 +60,13 @@ public class MethodReferencesDemo {
 		//method reference
 		Printable printable2 = methodReferencesDemo::display;
 		printable2.print("hello java");
+		System.out.println();System.out.println();
 		
 		
 		
-		// 3) reference to an instance object of arbitary object///////////////////////////////////////////////////////////////////////////////////////
+		
+		
+		// 3) reference to an instance object of arbitary object///////////////////////////////////////////////////////////////////////////////////////////////////
 		
 		Function<String, String> stringFunction = (ip) -> ip.toLowerCase();
 		System.out.println("stringFunction :: "+stringFunction.apply("VISAL"));
@@ -60,7 +74,36 @@ public class MethodReferencesDemo {
 		Function<String, String> stringFunction2 = String::toLowerCase;
 		System.out.println("stringFunction :: "+stringFunction2.apply("VISALAAA"));
 		
+		// eg2 
+		String[] strArray = {"A","E","I","O","U","a","e","i","o","u"};
+		Arrays.sort(strArray, (s1,s2) -> s1.compareToIgnoreCase(s2));
+		System.out.println("strArray :: "+Arrays.toString(strArray));
 		
+		String[] strArray2 = {"A","E","I","O","U","a","e","i","o","u"};
+		Arrays.sort(strArray2, String::compareToIgnoreCase);
+		System.out.println("strArray2 :: "+Arrays.toString(strArray2));
+		System.out.println();System.out.println();
+		
+		
+		
+		
+		
+		// 4) reference to the constructor///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		List<String> fruits = new ArrayList<>();
+		fruits.add("bananna");
+		fruits.add("apple");
+		fruits.add("apple");
+		fruits.add("mango");
+		fruits.add("watermelon");
+		
+		Function<List<String>, Set<String>> setFunction = (fruitsList) -> new HashSet<>(fruits);
+		System.out.println("setFunction :: "+setFunction.apply(fruits));
+		
+		
+		//using method reference
+		Function<List<String>, Set<String>> setFunctionMethodRef = HashSet::new;                      //// for new method reference
+		System.out.println("setFunctionMethodRef :: "+setFunctionMethodRef.apply(fruits));
 	}
 
 }
